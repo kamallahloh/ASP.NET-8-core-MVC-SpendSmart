e09f3a120e90e8beccb9eaedec38498b34b20b2e
Kamal Lahloh <kamal.lahloh@gmail.com>
2024-10-02 22:41:49 +0300

    Add button

--- SpendSmart/obj/Debug/net8.0/SpendSmart.AssemblyInfo.cs
+++ SpendSmart/obj/Debug/net8.0/SpendSmart.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpendSmart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc46c235d52c71f8ce24c5d95527a69dc966463e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9d45e328a14951a8d605d5368176c494aef6cb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpendSmart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpendSmart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
